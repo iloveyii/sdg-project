850db139a5d61373c4047f8a32baf4fb9de2d8f7
Hazrat Ali <iloveyii@yahoo.com>
2020-02-12 14:05:35 +0100

    Jumbotran with from control

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,16 +55,20 @@ class App extends React.Component {
 
                 <Jumbotron style={{textAlign: 'center'}}>
                     <Form>
-                        <Form.Row>
-                            <Form.Group as={Col}>
-                                <Form.Label className="margin20">Select file to upload</Form.Label>
-                                <Button
-                                    variant="primary"
-                                    onClick={() => null}>
-                                    Upload
-                                </Button>
-                            </Form.Group>
-                        </Form.Row>
+                        <Form.Group as={Row} controlId="formHorizontalEmail">
+                            <Form.Label column sm={2}>
+                                Email
+                            </Form.Label>
+                            <Col sm={10}>
+                                <Form.Control type="email" placeholder="Email"/>
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row}>
+                            <Col sm={{span: 10, offset: 2}}>
+                                <Button type="submit">Sign in</Button>
+                            </Col>
+                        </Form.Group>
                     </Form>
                 </Jumbotron>
             </Container>
