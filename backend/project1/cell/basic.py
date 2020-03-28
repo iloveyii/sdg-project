33d74b96a5e7e4fd4103bb5d02a4b17a43ca85c6
Hazrat Ali <iloveyii@yahoo.com>
2020-03-28 01:28:37 +0100

    Added DIR constants

--- backend/project1/cell/basic.py
+++ backend/project1/cell/basic.py
@@ -12,41 +12,45 @@ dir_path = Path().absolute()
 raw_path = PurePath.joinpath(dir_path, 'data/raw/')
 transformed_path = PurePath.joinpath(dir_path, 'data/transformed/')
 
+RAW_DIR = os.path.dirname(os.path.realpath(__file__)) + '/data/raw/'
+TRANSFORMED_DIR = os.path.dirname(os.path.realpath(__file__)) + '/data/transformed/'
+HEAT_MAP_DIR = os.path.dirname(os.path.realpath(__file__)) + '/data/heatmap/'
+GATED_DIR = os.path.dirname(os.path.realpath(__file__)) + '/data/gated/'
+
 
 class Basic:
     def __init__(self):
         # data directory path - please uncomment the following line if you are using Ubuntu
         # datadir = os.path.join(FlowCytometryTools.__path__[0], 'tests', 'data', 'Plate01')
-        datadir = os.path.dirname(os.path.realpath(__file__)) + '/data/raw/'
-        self.datafile = os.path.join(datadir, 'RFP_Well_A3.fcs')
+        self.datafile = os.path.join(RAW_DIR, 'RFP_Well_A3.fcs')
         print('Path to data file is : ' + self.datafile)
-        self.tsample = False
+        self.sample = False
         self.read_data()
 
     def read_data(self):
-        self.tsample = FCMeasurement(ID='Test Sample', datafile=self.datafile)
-        # self.tsample = self.tsample.transform('hlog', channels=['Y2-A', 'B1-A', 'V2-A'], b=500)
+        self.sample = FCMeasurement(ID='Test Sample', datafile=self.datafile)
+        # self.sample = self.sample.transform('hlog', channels=['Y2-A', 'B1-A', 'V2-A'], b=500)
 
     def plot_columns(self, col1):
-        self.tsample.plot(col1, bins=100, alpha=0.9, color='green')
+        self.sample.plot(col1, bins=100, alpha=0.9, color='green')
 
     def get_channel_names(self):
-        return self.tsample.channel_names
+        return self.sample.channel_names
 
     def get_meta(self):
-        return self.tsample.meta
+        return self.sample.meta
 
     def head(self):
         channelx = "HDR-T"
         channely = "FSC-A"
 
-        return (self.tsample.data[[channelx, channely]][:-10]).head(10)
+        return (self.sample.data[[channelx, channely]][:-10]).head(10)
 
     def medians(self):
         channelx = "HDR-T"
         channely = "FSC-A"
 
-        print((self.tsample.data[[channelx, channely]][:-10]).head(10))
+        print((self.sample.data[[channelx, channely]][:-10]).head(10))
         print(channelx, channely)
 
     def df_values_to_csv(self, df, out_file):
@@ -62,24 +66,24 @@ class Basic:
         file_handle.close()
         print('Data Saved in ', file_name, '... Sample = ', xstr)
 
+    # This function extracts data of the specified channels into a csv file
     def extract_channels_data(self, ichannelx=4, ichannely=6):
-            longfilename = 'RFP_Well_A3.fcs'
-            print("Reading .fcs file ", longfilename)
-            shortfilename = longfilename.split('.')[0]
-            sample = self.tsample
+        long_filename = 'RFP_Well_A3.fcs'
+        print("Reading .fcs file ", long_filename)
+        short_filename = long_filename.split('.')[0]
 
-            channelnames = sample.channel_names
-            channelx = channelnames[int(ichannelx) - 1]
-            channely = channelnames[int(ichannely) - 1]
+        channel_names = self.sample.channel_names
+        channelx = channel_names[int(ichannelx) - 1]
+        channely = channel_names[int(ichannely) - 1]
 
-            # Transformation is selected in the FE
+        # Transformation is selected in the FE
 
-            data1 = sample.data[channelx]
-            data2 = sample.data[channely]
-            data3 = pd.concat([data1, data2], axis=1)
-            outfile = shortfilename + '_out.csv'
-            self.df_values_to_csv(data3, outfile)
-            print('Raw Data Saved...', outfile)
+        data1 = self.sample.data[channelx]
+        data2 = self.sample.data[channely]
+        data3 = pd.concat([data1, data2], axis=1)
+        outfile = short_filename + '_out.csv'
+        self.df_values_to_csv(data3, outfile)
+        print('Raw Data Saved...', outfile)
 
 
 basic = Basic()
