05dd7704f977396ca745570aa9f362068115de2f
Hazrat Ali <iloveyii@yahoo.com>
2020-04-06 00:38:50 +0200

    Added urls

--- djangoweb/djangoweb/settings.py
+++ djangoweb/djangoweb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'data/raw')  # 'data' is my media folder
+MEDIA_URL = '/media/'
--- djangoweb/djangoweb/urls.py
+++ djangoweb/djangoweb/urls.py
@@ -1,7 +1,7 @@
-"""djangoweb URL Configuration
+"""project1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
+from django.conf.urls import include, url
+# print(url(r'^app1/', include('app1.url')),)
+from django.http import HttpResponse
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'name': 'CELL ANALYSIS'
+    }
+    return render(request, 'index.html', context=context)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Default page
+    url(r'^/?$', index),
+
+    # Path to api application,
+    url(r'^api/', include('api.url')),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
