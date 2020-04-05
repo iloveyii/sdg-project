c3c2177d46ec3f857d8f5480b532b917b0000940
Hazrat Ali <iloveyii@yahoo.com>
2020-04-05 13:57:18 +0200

    Added k-means, flowpeaks

--- backend/project1/mlearning/machinelearning.py
+++ backend/project1/mlearning/machinelearning.py
@@ -21,6 +21,7 @@ BIN_WIDTH = 100
 CSV_FILE = 'fcs_file.csv'
 FCS_FILE = 'RFP_Well_A3.fcs'
 FCS_FILE_A4 = 'CFP_Well_A4.fcs'
+ECOLI_FILE = 'ecoli.fcs'
 
 
 class MachineLearning:
@@ -68,17 +69,8 @@ class MachineLearning:
         savefig(png_file)
         print(type(ex2.data.head()))
 
-        # Clear prev sub plot
-        subplots(clear=True)
-        matplotlib.rc('figure', dpi=80)
-
-        ax = plt.subplot(111, frame_on=False)  # no visible frame
-        ax.xaxis.set_visible(False)  # hide the x axis
-        ax.yaxis.set_visible(False)  # hide the y axis
-
-        table(ax, ex2.data.head(), loc='center')  # where df is your data frame
         png_file = os.path.join(STATIC_DIR + '/img/', 'gausian_table.png')
-        savefig(png_file)
+        self.__df_to_png(ex2.data.head(), png_file)
 
         subplots(clear=True)
         g = flow.GaussianMixtureOp(name="Gauss",
@@ -93,10 +85,75 @@ class MachineLearning:
         png_file = os.path.join(STATIC_DIR + '/img/', 'gausian_posterior.png')
         savefig(png_file)
 
+        png_file = os.path.join(STATIC_DIR + '/img/', 'gausian_posterior_table.png')
+        self.__df_to_png(ex2.data.head(), png_file)
+
+        # We can use this second metadata column to filter out events with low posterior probabilities:
+        ex2.query("Gauss_1_posterior > 0.9 | Gauss_2_posterior > 0.9").data.head()
+        flow.HistogramView(channel="V2-A",
+                           huefacet="Gauss",
+                           scale="logicle",
+                           subset="Gauss_1_posterior > 0.9 | Gauss_2_posterior > 0.9").plot(ex2)
+        png_file = os.path.join(STATIC_DIR + '/img/', 'gausian_filtered_low_posterior.png')
+        savefig(png_file)
+
+        subplots(clear=True)
+        # Basic usage, assigning each event to one of the mixture components: (the isolines in the default_view() are 1, 2 and 3 standard deviations away from the mean.)
+        g = flow.GaussianMixtureOp(name="Gauss",
+                                   channels=["V2-A", "Y2-A"],
+                                   scale={"V2-A": "logicle",
+                                          "Y2-A": "logicle"},
+                                   num_components=2)
+
+        g.estimate(ex)
+        ex2 = g.apply(ex)
+        g.default_view().plot(ex2, alpha=0.1)
+        png_file = os.path.join(STATIC_DIR + '/img/', 'gausian_mixture_model_two_channels.png')
+        savefig(png_file)
+
+        subplots(clear=True)
+        # K-Means
+        k = flow.KMeansOp(name="KMeans",
+                          channels=["V2-A", "Y2-A"],
+                          scale={"V2-A": "logicle",
+                                 "Y2-A": "logicle"},
+                          num_clusters=2,
+                          by=['Dox'])
+
+        k.estimate(ex)
+        ex2 = k.apply(ex)
+        k.default_view(yfacet="Dox").plot(ex2)
+        png_file = os.path.join(STATIC_DIR + '/img/', 'k_means.png')
+        savefig(png_file)
+
+        # FlowPeaks
+        ecoli_file_path = os.path.join(RAW_DIR, FCS_FILE)
+        ex = flow.ImportOp(tubes=[flow.Tube(file=ecoli_file_path)]).apply()
+
+        flow.ScatterplotView(xchannel="FSC-A",
+                             xscale='log',
+                             ychannel="FSC-H",
+                             yscale='log').plot(ex)
+        png_file = os.path.join(STATIC_DIR + '/img/', 'flow_peaks.png')
+        savefig(png_file)
+
+        # K-MEANS2
+        k = flow.KMeansOp(name="KMeans",
+                          channels=["FSC-A", "FSC-H"],
+                          scale={"FSC-A": "log",
+                                 "FSC-H": "log"},
+                          num_clusters=3)
+
+        k.estimate(ex)
+        ex2 = k.apply(ex)
+        k.default_view().plot(ex2)
+        png_file = os.path.join(STATIC_DIR + '/img/', 'k_means2.png')
+        savefig(png_file)
+
     def __df_to_png(self, df, file_path):
         # Clear prev sub plot
         subplots(clear=True)
-        matplotlib.rc('figure', dpi=80)
+        matplotlib.rc('figure', dpi=160)
 
         ax = plt.subplot(111, frame_on=False)  # no visible frame
         ax.xaxis.set_visible(False)  # hide the x axis
