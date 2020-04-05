61697a76ac912084825e7c7e7f37f6bebe9ea98a
Hazrat Ali <iloveyii@yahoo.com>
2020-04-06 01:29:34 +0200

    Added container for plotting

---
+++ plotting/api.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Product(Resource) :
+    def get(self):
+        return {
+            'products' : [
+                'applen',
+                'banan',
+                'frukt',
+                'choklad',
+                'mjölk',
+                'blommor'
+            ]
+        }
+api.add_resource(Product, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
