140050cd24b779c060a5b8deb14c2dc7177dacd5
Hazrat Ali <iloveyii@yahoo.com>
2020-04-13 19:11:02 +0200

    Added React app

---
+++ djangoweb/api/static/precache-manifest.112db7c701d06b0f8be64b47dedd57ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c633632fde84c97d0d10fd81f7653962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a08bdaa3fd3e0b5d799b",
+    "url": "/static/js/2.8f563518.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f563518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d204f58f74cdc5ad3f69",
+    "url": "/static/js/main.f43cffe9.chunk.js"
+  },
+  {
+    "revision": "b89fc9643503cbdc47a9",
+    "url": "/static/js/runtime-main.c4e14da9.js"
+  }
+]);
