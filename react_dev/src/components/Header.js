be00856a41ad18b4d0262f2bf01001d02b0f1df9
Hazrat Ali <iloveyii@yahoo.com>
2020-04-13 17:22:06 +0200

    Added react deploy script

--- react_dev/src/components/Header.js
+++ react_dev/src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return (
             <div className="py-5 text-center">
-                <img src="/img/logo192.png" width="200" alt="img"/>
+                <img src="/static/img/logo192.png" width="200" alt="img"/>
                 <br/>
                 <a href="/api/logout" className="btn btn-warning">Logout</a>
             </div>
