9ac0c293c663166c4b42985a8d408ff927f1418b
Hazrat Ali <iloveyii@yahoo.com>
2020-05-09 13:54:27 +0200

    login css

--- plotting/api.py
+++ plotting/api.py
@@ -4,14 +4,21 @@ from flask_wtf.csrf import CSRFProtect
 from flask_restful import Resource, Api
 from plotting import Plotting
 
+from datetime import datetime
+
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 api = Api(app)
+
+dt = datetime.now()
+gl = {'test': 'this is a test', 'ts': dt.microsecond}
 
 
 class Data(Resource):
     @csrf.exempt
     def get(self):
+        global gl
+        # return gl
         file_id = request.args.get('id')
         if not file_id:
             file_id = 'hazrat_kth_se'
