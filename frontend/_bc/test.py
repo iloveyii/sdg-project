97d260c94d4ee9b65610bee403b24b271891aaed
Hazrat Ali <iloveyii@yahoo.com>
2020-05-23 15:44:32 +0200

    Code cleanup

--- frontend/_bc/test.py
+++
@@ -1,9 +0,0 @@
-x = [{'name': 'A06 Ut SY', 'id': 'A06 Ut SY.FCS'},
-     {'name': 'A02 Kranvatten kvall SYBR', 'id': 'A02 Kranvatten kvall SYBR.fcs'}]
-
-fileList = []
-for i in x:
-    print(i["id"])
-    fileList.append(i["id"])
-
-print(fileList)
