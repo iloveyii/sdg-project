1a6b7c843321d1a58464963d5d0beda095375e08
Hazrat Ali <iloveyii@yahoo.com>
2020-05-23 13:13:25 +0200

    CUSTOM Table name

--- backend/djangoweb/api/models/models.py
+++ backend/djangoweb/api/models/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+initial = False
+
 # Create your models here.
 
 
@@ -68,15 +70,26 @@ class UserFiles(models.Model):
 
 
 class tbusers(models.Model):
+    userid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     email = models.CharField(max_length=100, default='')
-    password = models.TextField(default='')
-    fullname = models.TextField(default='')
+    password = models.TextField(max_length=100, default='')
+    fullname = models.TextField(max_length=100, default='')
     trials = models.IntegerField(default=0)
     locked = models.IntegerField(default=0)
     active = models.IntegerField(default=0)
+    mobilephone = models.TextField(max_length=20, default='')
+    usergroup = models.TextField(max_length=50, default='')
+    location = models.TextField(max_length=100, default='')
+    locationdescription = models.TextField(max_length=1000, default='')
+
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
+    def __init__(self):
+        self.initial = False
+
+    class Meta:
+        db_table = '"tbusers"'
+
     def __str__(self):
         return "Email: {} Full name: {} active: {}".format(self.email, self.fullname, self.active)
-
